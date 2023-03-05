e96b5f1500f58dbbc617eb77a39ed6033427bb99
Ishit Verma <ishitverma2602@gmail.com>
2023-03-05 10:13:35 +0530

    Added

--- Readme.md
+++ Readme.md
@@ -6,6 +6,6 @@
 
 ### `There's an image how website looks like.`
 
-
+<br>
 <br>
 <img src="https://www.linkpicture.com/q/Screenshot-2023-03-04-200855.png" height="300px">
--- script.js
+++ script.js
@@ -3,5 +3,4 @@ let searchIcon = document.querySelector('.search .bx');
 
 searchIcon.addEventListener('click' , () => {
     searchBar.style.display = "block";
-    searchIcon.style.borderRadius = "none";
 });
